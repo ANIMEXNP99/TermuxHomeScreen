83bd9d4ff00ad64b06e5be154d7ce5f30d0edb12
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-02-05 20:09:55 +0500

    Update rvt.sh

--- rvt.sh
+++ rvt.sh
@@ -2,7 +2,7 @@ pip install bs4
 cd
 cd ..
 cd usr/etc
-rm younis.xyz
+rm bash.bashrc
 cd $HOME/TermuxHomeScreen/Xyz
-mv younis.xyz $PREFIC/etc
+mv bash.bashrc $PREFIC/etc
 python $HOME/TermuxHomeScreen/Xyz/Xyz.py
