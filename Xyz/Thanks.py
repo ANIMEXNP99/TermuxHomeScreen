47f489549155d79919aed46a7982ca4cf36fac97
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-02-05 19:48:38 +0500

    Add files via upload

---
+++ Xyz/Thanks.py
@@ -0,0 +1,28 @@
+import time
+import json
+import random
+import string
+import os, sys
+import requests
+import collections
+import urllib.request
+from bs4 import BeautifulSoup
+
+nu = '\033[0m'
+re = '\033[1;31m'
+gr = '\033[1;32m'
+cy = '\033[1;36m'
+
+def write(in_text):
+	for char in in_text:
+		time.sleep(0.1)
+		sys.stdout.write(char)
+		sys.stdout.flush()
+write(f"\n{gr}[+]{re} Thanks for using Younis john tools...")
+print("\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print("\n  ~ MY SOCIAL MEDIA  ~")
+print("WhatsApp : +923404708884")
+print("Github : https://github.com/YounisXyz")
+print("Facebook : https://www.facebook.com/xyzhackers")
+print("\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print('\n')
