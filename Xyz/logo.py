fdd7a7c9ec14a68ea365140d3d393ce7b1a896b6
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-02-05 10:17:24 +0500

    Add files via upload

---
+++ Xyz/logo.py
@@ -0,0 +1,28 @@
+import time
+import json
+import random
+import string
+import os, sys
+import requests
+import collections
+import urllib.request
+from bs4 import BeautifulSoup
+
+nu = '\033[0m'
+re = '\033[1;31m'
+gr = '\033[1;32m'
+cy = '\033[1;36m'
+
+def write(in_text):
+	for char in in_text:
+		time.sleep(0.1)
+		sys.stdout.write(char)
+		sys.stdout.flush()
+write(f"\n{gr}[+]{re} Thanks for using YounisXyz tools...")
+print("\n------------------------")
+print(f"\n{cy} \r ~ Social Media ~")
+print(f"{green} https://wa.me/+923404708884") 
+print(f"{green} Github : https://github.com/YounisXyz") 
+print(f"{green} Facebook : https://www.facebook.com/xyzhackers")
+print("\n------------------------")
+print('\n')
